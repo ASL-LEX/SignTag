3e1f5d4bd062e1873efaef81414a52def56f72a2
Collin Bolles <collinbolles@gmail.com>
2026-02-22 19:40:36 -0500

    Add in placeholder API descriptors for project api

--- packages/server/src/project/controllers/project-admin.controller.ts
+++ packages/server/src/project/controllers/project-admin.controller.ts
@@ -1,19 +1,26 @@
 import { Controller, Delete, Post, Put, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('api/admin/project')
+@ApiTags('Project (Admin)')
 export class AdminProjectController {
   @Post()
+  @ApiOperation({ summary: 'Create a new project' })
   async create() {}
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a single project' })
   async get() {}
 
   @Get()
-  async getAll() {}
+  @ApiOperation({ summary: 'Get many projects' })
+  async getMany() {}
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update an existing project' })
   async update() {}
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete an existing project' })
   async delete() {}
 }
