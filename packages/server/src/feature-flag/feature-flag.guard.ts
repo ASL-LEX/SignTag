50b950ac3f765a32c22078c433a2e6fece585912
Collin Bolles <collinbolles@gmail.com>
2026-02-22 20:14:53 -0500

    Add a feature flag to block use of in-dev features

---
+++ packages/server/src/feature-flag/feature-flag.guard.ts
@@ -0,0 +1,34 @@
+import { CanActivate, ExecutionContext, Injectable, NotImplementedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { FeatureFlag } from './feature-flag.decorator';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class FeatureFlagGuard implements CanActivate {
+  constructor (private readonly reflector: Reflector, private readonly configService: ConfigService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const targetFeatureFlagName = this.reflector.get(FeatureFlag, context.getHandler());
+    const failureState = new NotImplementedException('This feature is not currently enabled');
+
+    // If no target feature is applied, then assume no feature flag
+    // is gating this endpoint
+    if (targetFeatureFlagName === undefined) {
+      return true;
+    }
+
+    // Next try to find a matching environment variable from the config service
+    const featureFlagValue = this.configService.get<string>(targetFeatureFlagName);
+
+    // If the value is undefined then we are considering the feature flag as being false
+    if (featureFlagValue === undefined) {
+      throw failureState;
+    }
+
+    // Otherwise we need the flag value to be some capitalization of 'true'
+    if (featureFlagValue.toLowerCase() === 'true') {
+        return true;
+    }
+    throw failureState;
+  }
+}
--- packages/server/src/project/controllers/project-admin.controller.ts
+++ packages/server/src/project/controllers/project-admin.controller.ts
@@ -1,26 +1,32 @@
 import { Controller, Delete, Post, Put, Get } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { FeatureFlag } from '../../feature-flag/feature-flag.decorator';
 
 @Controller('api/admin/project')
 @ApiTags('Project (Admin)')
 export class AdminProjectController {
   @Post()
   @ApiOperation({ summary: 'Create a new project' })
+  @FeatureFlag('SIGNTAG_ADMIN_PROJECT_ENDPOINT')
   async create() {}
 
   @Get(':id')
   @ApiOperation({ summary: 'Get a single project' })
+  @FeatureFlag('SIGNTAG_ADMIN_PROJECT_ENDPOINT')
   async get() {}
 
   @Get()
   @ApiOperation({ summary: 'Get many projects' })
+  @FeatureFlag('SIGNTAG_ADMIN_PROJECT_ENDPOINT')
   async getMany() {}
 
   @Put(':id')
   @ApiOperation({ summary: 'Update an existing project' })
+  @FeatureFlag('SIGNTAG_ADMIN_PROJECT_ENDPOINT')
   async update() {}
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete an existing project' })
+  @FeatureFlag('SIGNTAG_ADMIN_PROJECT_ENDPOINT')
   async delete() {}
 }
