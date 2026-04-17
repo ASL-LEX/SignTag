cf423ce305f4da8115279b7eeafdb9f3f71f8881
Collin Bolles <collinbolles@gmail.com>
2026-04-17 12:52:32 -0400

    Working download script

---
+++ packages/scripts/downloading/README.md
@@ -0,0 +1,8 @@
+
+# Steps
+
+1. Download and install gcloud https://docs.cloud.google.com/sdk/docs/install-sdk
+2. Select the `signlab-eu-prod` GCP project
+```
+gcloud config set project signlab-eu-prod
+```
--- packages/scripts/downloading/entry-download.py
+++ packages/scripts/downloading/entry-download.py
@@ -3,14 +3,20 @@ from pathlib import Path
 import logging
 import os
 import json
+from google.cloud import storage
 
 
 def main():
     parser = ArgumentParser('Utility to download entry videos/images from a GCP bucket')
+    parser.add_argument('bucket',
+                        help='GCP bucket where the entries are stored')
     parser.add_argument('entries',
                         help='JSON file of entries to download')
     parser.add_argument('output',
                         help='Directory to store all of the entries')
+    parser.add_argument('--overwrite',
+                        help='Will replace found files from bucket',
+                        action='store_true')
     parser.add_argument('--log-level',
                         choices=logging._nameToLevel.keys(),
                         help='Log output level',
@@ -35,7 +41,32 @@ def main():
     # Read in the entries to load
     with open(entry_list_path, 'r') as entry_list_file:
         entries = json.load(entry_list_file)['entries']
-    logging.debug(f'Found {len(entries)} number of entries to download')
+    logging.info(f'Found {len(entries)} number of entries to download')
+
+    # Get the GCP storage client
+    storage_client = storage.Client()
+    bucket = storage_client.lookup_bucket(args.bucket)
+    if bucket is None:
+        logging.error(f'Could not get the bucket "{bucket}", please verify you are logged in and have the right bucket name')
+        exit(1)
+    logging.debug(f'Found bucket: {bucket.name}')
+
+    # Now we can loop over all the entries
+    for entry_key in entries:
+        filename = Path(entry_key).name
+        output_location = entry_output_folder / filename
+
+        # Check if we should skip the given entry
+        if output_location.exists() and args.overwrite == False:
+            logging.info(f'Skipping {output_location}')
+            continue
+
+        # Otherwise we download the file
+        blob = bucket.blob(entry_key)
+        blob.download_to_filename(output_location.absolute())
+        logging.info(f'Downloaded {output_location}')
+
+
 
 if __name__ == '__main__':
     main()
