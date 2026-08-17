61d3768f1ca00afe4020cbcb2a440adf2fb98d34
Collin Bolles <collinbolles@gmail.com>
2026-08-17 12:26:32 -0400

    Add creation endpoint

--- packages/server/src/project/controllers/project-admin.controller.ts
+++ packages/server/src/project/controllers/project-admin.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Delete, Post, Put, Get, Query, Response } from '@nestjs/common';
+import { Controller, Delete, Post, Put, Get, Query, Response, Body } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { FeatureFlag } from '../../feature-flag/feature-flag.decorator';
 import { RAQuery } from '../../shared/ra-query.dto';
 import { Response as Res } from 'express';
 import { ProjectService } from '../project.service';
 import { makeContentRange } from '@bu-sail/ra-query-core';
+import { ProjectCreateV2 } from '../dtos/create.dto';
+import { Project } from '../project.model';
 
 @Controller('api/admin/project')
 @ApiTags('Project (Admin)')
@@ -14,12 +16,19 @@ export class AdminProjectController {
   @Post()
   @ApiOperation({ summary: 'Create a new project' })
   @FeatureFlag('SIGNTAG_ADMIN_PROJECT_ENDPOINT')
-  async create() {}
+  async create(@Body() newProject: ProjectCreateV2): Promise<Project> {
+    // TODO: Check permissions
+
+    // TODO: Get organization ID from auth guard
+    return this.projectService.create(newProject, '6a832a3d17d5333f9f601db3')
+  }
 
   @Get()
   @ApiOperation({ summary: 'Get many projects' })
   @FeatureFlag('SIGNTAG_ADMIN_PROJECT_ENDPOINT')
   async getMany(@Query() query: RAQuery, @Response() response: Res): Promise<any> {
+    // TODO: Check permissions
+
     // TODO: Get organization ID from auth guard
     const projects = await this.projectService.get(query, '6a832a3d17d5333f9f601db3');
 
--- packages/server/src/project/dtos/create.dto.ts
+++ packages/server/src/project/dtos/create.dto.ts
@@ -1,5 +1,14 @@
 import { InputType, OmitType } from '@nestjs/graphql';
 import { Project } from '../project.model';
+import { ApiProperty } from '@nestjs/swagger';
 
 @InputType()
 export class ProjectCreate extends OmitType(Project, ['_id', 'created', 'organization'] as const, InputType) {}
+
+export class ProjectCreateV2 implements Omit<Project, '_id' | 'created' | 'organization'> {
+  @ApiProperty({ description: 'Human readable name to identify the project' })
+  name!: string;
+
+  @ApiProperty({ description: 'Description of the project for easier recognition' })
+  description!: string;
+}
